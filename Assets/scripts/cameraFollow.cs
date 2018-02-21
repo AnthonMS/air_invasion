ae69e9b0abaf9b873017af3f5f3f2e79a47fc08d
madsh <mads.heilberg@gmail.com>
2018-02-21 11:44:05 +0100

    Changes made to cameraFollow -> no movement on Y axis

--- Assets/scripts/cameraFollow.cs
+++ Assets/scripts/cameraFollow.cs
@@ -6,23 +6,27 @@ public class cameraFollow : MonoBehaviour {
 
     private GameObject player;
     private Vector3 offset;
+    private Vector3 playerPos;
+    private float transformX;
 
     // Use this for initialization
     void Start ()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         offset = transform.position - player.transform.position;
+
+        playerPos = player.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+	
 	}
 
     // LateUpdate is called after Update each frame
     private void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = new Vector3(player.transform.position.x + offset.x, playerPos.y + offset.y, player.transform.position.z + offset.z);
     }
 }
