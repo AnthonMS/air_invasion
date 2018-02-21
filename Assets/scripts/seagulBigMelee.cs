c71b16caa0a3283b3d3285c18c74a12244b745e7
Noahtyr <Noahdavidian@hotmail.dk>
2018-02-21 10:39:04 +0100

    This is the first commit
    Lets test this bad boi !

---
+++ Assets/scripts/seagulBigMelee.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seagulBigMelee : MonoBehaviour {
+
+
+    public float speed = 5;
+    public float damage = 15;
+    public float xOffset = 5;
+    public float health = 20;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
