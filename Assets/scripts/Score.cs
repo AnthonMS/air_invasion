ad02e46897f4d2f184ee90fb735ea8e67b66b6b2
flyberson <flyberson12@gmail.com>
2018-02-28 13:11:35 +0100

    added ui

--- Assets/scripts/Score.cs
+++ Assets/scripts/Score.cs
@@ -1,10 +1,33 @@
-﻿using System;
-namespace AssemblyCSharp.scripts
-{
-    public class Score
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public int scorenum;
+    public Text scoreText;
+
+
+    public Score()
     {
-        public Score()
-        {
-        }
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        scorenum = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void updateScore(){
+        Debug.Log("updating score");
+        scorenum++;
+        scoreText.text="score="+scorenum;
+        
     }
 }
