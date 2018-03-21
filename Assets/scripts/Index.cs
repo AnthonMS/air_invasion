a006c216d78bcfbaea15eb67d10a2a7b945e401a
madsh <mads.heilberg@gmail.com>
2018-03-21 14:42:18 +0100

    input button changes

---
+++ Assets/scripts/Index.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Index : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Jump()
+    {
+        GameObject.FindGameObjectWithTag("Player").SendMessage("ClickButtonNew", true);
+        Debug.Log("jump");
+    }
+
+    public void Protect(bool value)
+    {
+        GameObject.FindGameObjectWithTag("Player").SendMessage("ClickProtectButton", value);
+
+    }
+}
